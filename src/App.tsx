9a5cb48fba56d79220e6ec86af0af38a00bf3c19
neoki <ot07neoki@gmail.com>
2024-02-03 22:38:26 +0900

    chore: setup @shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="mx-auto min-h-screen w-full max-w-[40rem] p-8">
+      <h1 className="text-center text-3xl font-bold">Salary Flows Lite</h1>
+    </main>
+  );
 }
 
 export default App;
